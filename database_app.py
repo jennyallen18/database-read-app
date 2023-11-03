50550572b72f001756895a8b3fd2e8647415cbaa
jennyallen18 <149631750+jennyallen18@users.noreply.github.com>
2023-11-03 15:52:38 -0700

    Update database_app.py
    Print table to html

--- database_app.py
+++ database_app.py
@@ -6,10 +6,6 @@ import streamlit as st
 
 def get_connection():
     conn_string = "host='hh-pgsql-public.ebi.ac.uk' dbname='pfmegrnargs' user='reader' password='NWDMCE5xdipIjRrp'"
-    #conn = psycopg2.connect(conn_string)
-    #cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-
-    #engine = sqlalchemy.create_engine(conn_string)
 
     engine = sqlalchemy.create_engine("postgresql+psycopg2://reader:NWDMCE5xdipIjRrp@hh-pgsql-public.ebi.ac.uk/pfmegrnargs")
     
@@ -17,10 +13,9 @@ def get_connection():
     query = "SELECT * FROM rnc_database"
 
     df = pd.read_sql_query(query, engine)
-    print(df)
-    #cursor.execute(query)
-    #for row in cursor:
-     #   print(row)
+    df.to_html(header="true", table_id="table")
+    #print(df)
+   
 
 
 if __name__ == "__main__":
